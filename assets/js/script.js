c16492873c71550293cde097c047bb646c57c788
Laplace <lap2837@outlook.com>
2024-04-01 22:40:01 -0600

    Added the modal body and a datepicker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -34,5 +37,10 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
-});
+    // printProjectData();
+  
+//     // ? Make lanes droppable
+//     $('.lane').droppable({
+//       accept: '.draggable',
+//       drop: handleDrop,
+    });
